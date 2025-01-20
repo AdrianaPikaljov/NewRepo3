39fec92ba11aa75ba13331518f8caf6d662ec81a
opilane <opilane@HP-CZC2349HTZ>
2025-01-20 11:30:37 +0200

    Add project files.

---
+++ põhikonstruktsioonid.py
@@ -0,0 +1,52 @@
+print("hello epta 1","hello epta 2",end="...",sep="---")
+
+#sep  eto  mezhdu slovami
+#end eto posle vsego
+#andmetüübid 
+#täisarv- int, ujukomaarv - float, sõne - str, tõevärtused - bool
+#astendamine **
+print()
+a=5
+aste=2
+tulemus=a**aste
+print(a,"**",aste,"=",tulemus)
+#korrutamine *
+print()
+arv1=int(input("esimene korrutaja: "))
+arv2=int(input("teine korrutaja: "))
+tulemus2=arv1*arv2
+print(arv1,"*",arv2,"=",tulemus2)
+#jagamine /
+print()
+arv1=int(input("esimene arv: "))
+arv2=int(input("jagaja: "))
+tulemus3=arv1/arv2
+print(arv1,"/",arv2,"=",tulemus3)
+
+#jagamisjääk %
+print()
+arv1=int(input("esimene arv: "))
+arv2=int(input("jagaja: "))
+tulemus4=arv1%arv2
+print(arv1,"%",arv2,"=",tulemus4)
+
+#liitmine +
+print()
+arv1=int(input("esimene arv: "))
+arv2=int(input("teine arv: "))
+tulemus5=arv1+arv2
+print(arv1,"+",arv2,"=",tulemus5)
+
+#lahutamine - 
+print()
+arv1=int(input("esimene arv: "))
+arv2=int(input("teine arv: "))
+tulemus6=arv1-arv2
+print(arv1,"-",arv2,"=",tulemus6)
+
+#jagamise täisarvulise osa leidmine //
+print()
+arv1=int(input("esimene arv: "))
+arv2=int(input("jagaja: "))
+tulemus7=arv1//arv2
+print(arv1,"//",arv2,"=",tulemus7)
